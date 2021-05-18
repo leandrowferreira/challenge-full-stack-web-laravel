5bba53300627a9cfa3580c6a19931f5bed7691c5
Leandro Ferreira <leandrowferreira@gmail.com>
2021-05-17 21:05:25 -0300

    Ajusts required by Sanctum to perform SPA Auth

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
@@ -42,6 +42,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace('App\\Http\\Controllers')
+                ->group(base_path('routes/web.php'));
         });
     }
 
